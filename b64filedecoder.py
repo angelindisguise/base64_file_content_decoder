3cf2fec52e871045a766046a9ead0e6855a9e1c4
Jordan <76496683+angelindisguise@users.noreply.github.com>
2024-04-22 20:51:56 +0000

    Create b64filedecoder.py
    Written in a kali linux environment

---
+++ b64filedecoder.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import base64
+
+filename = input("Provide file name/path: ").strip()  # No need for str() around input
+iterations = int(input("Enter number of iterations (times you would like to decode the contents of the file): "))
+finished = False
+
+try:
+    with open(filename, 'r') as file:
+        content = file.read()
+
+    for i in range(iterations):
+        content = base64.b64decode(content).decode('utf-8')  # Update content with decoded value
+
+    with open("b64decoded.txt", 'w') as decoded:
+        decoded.write(content)
+        print("Successfully decoded file content.")
+
+except FileNotFoundError:
+    print(f"File: {filename}, does not appear to exist...")
+except (PermissionError, IOError) as e:
+    print(f"Error accessing file: {e}")
